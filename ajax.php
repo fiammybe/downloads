9b6fe8c32bc5f2075f80846a678056ba0136b312
Steffen Floher <qm-b@hotmail.de>
2012-02-24 07:54:45 +0000

    test for ajax.php
    git-svn-id: http://amaryllis-modules.googlecode.com/svn/trunk/downloads@433 66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad

--- ajax.php
+++ ajax.php
@@ -61,11 +61,8 @@ if (in_array($clean_op, $valid_op, TRUE)) {
 			$downloadObj->setVar("download_downcounter", ((int)($down_counter) + 1));
 			$downloadObj->store(TRUE);
 			
-			if((strpos(xoops_getenv('HTTP_REFERER'), ICMS_URL) !== FALSE) ) {
-				return redirect_header (DOWNLOADS_URL . 'singledownload.php?op=downfile&amp;download_id=' . $download_id, 3, _MD_DOWNLOADS_DOWNLOAD_START);
-			} else {
-				return redirect_header (DOWNLOADS_URL . 'singledownload.php?download_id=' . $download_id, 3, _NO_PERM);
-			}
+			return redirect_header (DOWNLOADS_URL . 'singledownload.php?op=downfile&amp;download_id=' . $download_id, 3, _MD_DOWNLOADS_DOWNLOAD_START);
+			
 			break;
 			
 		case 'getFileMirror':
@@ -93,11 +90,8 @@ if (in_array($clean_op, $valid_op, TRUE)) {
 			$downloadObj->setVar("download_downcounter", ((int)($down_counter) + 1));
 			$downloadObj->store(TRUE);
 			
-			if((strpos(xoops_getenv('HTTP_REFERER'), ICMS_URL) !== FALSE) ) {
-				return redirect_header (DOWNLOADS_URL . 'singledownload.php?op=downfileMirror&amp;download_id=' . $download_id, 3, _MD_DOWNLOADS_DOWNLOAD_START);
-			} else {
-				return redirect_header (DOWNLOADS_URL . 'singledownload.php?download_id=' . $download_id, 3, _NO_PERM);
-			}
+			return redirect_header (DOWNLOADS_URL . 'singledownload.php?op=downfileMirror&amp;download_id=' . $download_id, 3, _MD_DOWNLOADS_DOWNLOAD_START);
+			
 			break;
 			
 		case 'addreview':
